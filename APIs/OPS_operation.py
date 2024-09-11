e8d7a920c3575ad639b0e2531b6dcc9f0a5561c1
Shawn Xiao <shawn.xiao@osl.com>
2024-09-11 17:52:46 +0800

    combine the crypto and fiat deposit depends on condition of currency_type

--- APIs/OPS_operation.py
+++ APIs/OPS_operation.py
@@ -1,3 +1,4 @@
+from APIs.OPS_console.OPS_APIs import OPS_API
 from APIs.OPS_console.OPS_Approval import OPS_Approval
 from APIs.OPS_console.OPS_Public import OpsPublic
 from APIs.OPS_console.OPS_deposit import OPS_Deposit
@@ -10,23 +11,25 @@ class OPS_operation:
         pass
 
     def coin_deposit(self, currency, amount, account=None, uuid=None):
-        deposit = OPS_Deposit()
-        id = deposit.coin_deposit(currency, amount, account, uuid)['uuid']
-        uuid = deposit.get_attribute('uuid')
+        Deposit_Obj = OPS_Deposit()
+        approval_id = Deposit_Obj.deposit(currency, amount, account, uuid)['uuid']
+        uuid = Deposit_Obj.get_attribute('uuid')
+        currency_type = Deposit_Obj.get_attribute('currency_type')
         balance_before = OpsPublic().account_balance(uuid, currency)
-        approval = OPS_Approval().ManualCoinPaymentDeposit_list()[0]
-        print(approval)
-        assert approval['approvalRequest']['id'] == id, f"Approval ID not match, {approval['approvalRequest']['id']} not match {id}"
-        if account:
-            assert approval['manualCoinPaymentDepositDetail']['username'] == account, f"Approval ID not match, {approval['manualCoinPaymentDepositDetail']['username']} not match {account}"
-        elif uuid:
-            assert approval['manualCoinPaymentDepositDetail']['userUuid'] == uuid, f"uuid not match, {approval['manualCoinPaymentDepositDetail']['userUuid']} not match {uuid}"
-        assert RTD(approval['manualCoinPaymentDepositDetail']['amount']) == RTD(amount), f"amount not match, {RTD(approval['manualCoinPaymentDepositDetail']['amount'])} not match {RTD(amount)}"
-        assert approval['manualCoinPaymentDepositDetail']['ccy'] == currency.upper(), f"ccy not match, {approval['manualCoinPaymentDepositDetail']['ccy']} not match {currency.upper()}"
-        OPS_Approval().ManualCoinPaymentDeposit_approve(id)
+        Approval_Obj = OPS_Approval()
+        approval = Approval_Obj.approvalList(currency_type)[0]
+        # 检查审批信息
+        Approval_Obj.assertion_approvalList(approval_id, approval, account, uuid, amount, currency, currency_type)
+        # 审批操作
+        OPS_Approval().ticket_approve(currency_type, approval_id)
+        # 校验余额增加
         balance_after = OpsPublic().account_balance(uuid, currency)
+        logger.log(f'{balance_before}', 'critical')
+        logger.log(f'{balance_after}', 'critical')
+
         assert RTD(balance_before) + RTD(amount) == RTD(balance_after), f"balance incorrect, balance before:{RTD(balance_before)} + deposit amount:{RTD(amount)} == balance after:{RTD(balance_after)}"
         logger.log(f'Account:[{account}] Uuid:[{uuid}] Currency:[{currency}] Amount:[{amount}] Coin deposit successful')
 
 if __name__ == '__main__':
-    OPS_operation().coin_deposit('BTC',100,'shawn.xiao@osl.com')
+    OPS_API().ops_authToken()
+    OPS_operation().coin_deposit('HKD',100,'shawn.xiao@osl.com')
