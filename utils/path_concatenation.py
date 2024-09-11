d981dc3df175d1c2b61c0e093ad333b1f71c1a80
Shawn Xiao <shawn.xiao@osl.com>
2024-09-11 09:15:28 +0800

    concatenate with get method request

---
+++ utils/path_concatenation.py
@@ -0,0 +1,23 @@
+def get_concatenation(params):
+    path = ''
+    if params is not None:
+        for key, value in params.items():
+            if path == '':
+                path = '?'
+            if path != '?':
+                path = path + "&"
+            if isinstance(value, list):
+                for i in value:
+                    path = path + key + "=" + str(i) + "&"
+                path = path.rstrip('&')
+            elif value != '':
+                path = path + key + "=" + str(value)
+    return path
+
+if __name__ == '__main__':
+    data = {
+        "limit": 20,
+        "loginUsernameSearchText": 'xzx',
+        "verificationState": ["VERIFIED", "KYC_VERIFIED", "CRYPTO_VERIFIED"],
+    }
+    print(get_concatenation(data))
